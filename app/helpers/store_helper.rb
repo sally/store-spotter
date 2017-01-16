e967941c492b888fbd1ef8819ba570719f7f4e2e
Young Ju Park <parkyngj@gmail.com>
2017-01-16 04:26:48 -0800

    Add get closest store method for StoreHelper store search

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -4,7 +4,7 @@ class StoreHelper
 
   def find_store(input_address)
     @input_address = GeocoderService.address_info(Geocoder.search(input_address))
-    @store_candidates = filter_stores
+    get_closest(filter_stores)
   end
 
   private
@@ -18,6 +18,21 @@ class StoreHelper
     Store.all
   end
 
-  
+  def get_closest
+    input_coords = [@input_address['lat'], @input_address['lng']]
+
+    closest_store = store_candidates.first
+    closest_distance = Haversine.distance(input_coords, [closest_store.latitude, closest_store.longitude].map(&:to_f)).to_miles
+
+    store_candidates.each do |store|
+      distance = Haversine.distance(input_coords, [store.latitude, store.longitude].map(&:to_f)).to_miles
+      if closest_distance > distance
+        closest_store = store
+        closest_distance = distance
+      end
+    end
+
+    closest_store
+  end
 
 end
