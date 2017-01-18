631ab56fc454a61eaf210b4d81b7869c013ee404
Young Ju Park <parkyngj@gmail.com>
2017-01-18 15:10:27 -0800

    Remove store filter

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,34 +1,21 @@
 class StoreHelper
 
-  @@filters = ["zip_code", "city", "county", "state"]
-
   def find_store(input_address)
     @input_address = GeocoderService.address_info(Geocoder.search(input_address))
     return if @input_address.nil?
-    get_closest(filter_stores)
+    get_closest
   end
 
   private
 
-  def filter_stores
-    @@filters.each do |filter|
-      stores = Store.where("#{filter} ~* ? AND state ~* ?",
-                           "(#{@input_address[filter]}.*)",
-                           "(#{@input_address['state']}.*)")
-      return stores if !stores.empty?
-    end
-
-    Store.all
-  end
-
-  def get_closest(store_candidates)
+  def get_closest
     input_coords = [@input_address['lat'], @input_address['lng']]
-    closest_store = store_candidates.first
+    closest_store = Store.all.first
     closest_distance = Haversine.distance(input_coords,
                                           [closest_store.latitude, closest_store.longitude].map(&:to_f)
                                           ).to_miles
 
-    store_candidates.each do |store|
+    Store.all.each do |store|
       distance = Haversine.distance(input_coords,
                                     [store.latitude, store.longitude].map(&:to_f)
                                     ).to_miles
